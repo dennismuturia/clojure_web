4b2f289793516754d800e69a1be3bf6cd2586add
dennismuturia <dmuturia@mo-de.local>
2018-09-26 12:35:01 +0300

    Simple edits on the file

--- src/script/run.clj
+++ src/script/run.clj
@@ -1,4 +1,3 @@
 (use 'ring.adapter.jetty)
 (require 'web_clojure.core)
-
 (run-jetty #'web-clojure.core/app {:port 8080})
--- src/web_clojure/core.clj
+++ src/web_clojure/core.clj
@@ -7,13 +7,13 @@
 ;;Now we are creating a view
 (defn view-layout [& content]
   (html
-  (doctype :xhtml-strict)
-  (xhtml-tag "en"
-  [:head 
-    [:meta {:http-equiv "content-type" :content "text/html; charset=utf-8"}]
-    [:title "Simple webapp"]]
-    [:body content]
-  )))
+   (doctype :xhtml-strict)
+   (xhtml-tag "en"
+    [:head 
+      [:meta {:http-equiv "content-type" :content "text/html; charset=utf-8"}]
+      [:title "Simple webapp"]]
+    [:body content])))
+  
 ;;Lets create a form that will get the userdata
 (defn view-input []
   (view-layout
@@ -21,34 +21,34 @@
     [:form {:method "post" action "/"}
       [:input.math {:type "text" :name "a"}][:span.math "+"]
       [:input.math {:type "text" :name"b"}][:br]
-      [:input.action {:type "submit" :value "add"}]
-    ]
-  )
-)
+      [:input.action {:type "submit" :value "add"}]]))
+    
+  
+
 ;;Shows the output of the sum 
 (defn view-output [a b sum]
   (view-layout
     [:h2 "The sum of two numbers are....."]
     [:p.math a "+" b "=" sum]
-    [:a.action {:'href "/"} "add more numbers"]
-  )
-)
+    [:a.action {:'href "/"} "add more numbers"]))
+  
+
 
 ;;parse the input
 (defn parse-input [a b]
-  [(Integer/parseInt a) (Integer/parseInt b)]
-)
+  [(Integer/parseInt a) (Integer/parseInt b)])
+
 
 ;;Setting the routes
 (defroutes app
   (GET "/" []
-    (view-input)
-  )
+    (view-input))
+  
   (POST "/" [a b]
     (let [[a b] (parse-input a b) sum (+ a b)]
-      (view-output a b sum))
-  )
-)
+      (view-output a b sum))))
+  
+
 
 
 
