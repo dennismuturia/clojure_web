8d8b31b05bbd31c32cb3ecf681cb4fb11f6997de
dennismuturia <dmuturia@mo-de.local>
2018-09-26 12:53:42 +0300

    Changed the code to make it simple to read.

--- src/web_clojure/core.clj
+++ src/web_clojure/core.clj
@@ -7,48 +7,37 @@
 ;;Now we are creating a view
 (defn view-layout [& content]
   (html
-   (doctype :xhtml-strict)
-   (xhtml-tag "en"
-    [:head 
-      [:meta {:http-equiv "content-type" :content "text/html; charset=utf-8"}]
-      [:title "Simple webapp"]]
-    [:body content])))
-  
-;;Lets create a form that will get the userdata
+    (doctype :xhtml-strict)
+    (xhtml-tag "en"
+      [:head
+        [:meta {:http-equiv "Content-type"
+                :content "text/html; charset=utf-8"}]
+        [:title "adder"]]
+      [:body content])))
+
 (defn view-input []
   (view-layout
-    [:h2 "Let us add two numbers"]
-    [:form {:method "post" action "/"}
-      [:input.math {:type "text" :name "a"}][:span.math "+"]
-      [:input.math {:type "text" :name"b"}][:br]
+    [:h2 "add two numbers"]
+    [:form {:method "post" :action "/"}
+      [:input.math {:type "text" :name "a"}] [:span.math " + "]
+      [:input.math {:type "text" :name "b"}] [:br]
       [:input.action {:type "submit" :value "add"}]]))
-    
-  
 
-;;Shows the output of the sum 
 (defn view-output [a b sum]
   (view-layout
-    [:h2 "The sum of two numbers are....."]
-    [:p.math a "+" b "=" sum]
-    [:a.action {:'href "/"} "add more numbers"]))
-  
+    [:h2 "two numbers added"]
+    [:p.math a " + " b " = " sum]
+    [:a.action {:href "/"} "add more numbers"]))
 
-
-;;parse the input
 (defn parse-input [a b]
   [(Integer/parseInt a) (Integer/parseInt b)])
 
-
-;;Setting the routes
 (defroutes app
   (GET "/" []
     (view-input))
-  
+
   (POST "/" [a b]
-    (let [[a b] (parse-input a b) sum (+ a b)]
+    (let [[a b] (parse-input a b)
+          sum   (+ a b)]
       (view-output a b sum))))
   
-
-
-
-
